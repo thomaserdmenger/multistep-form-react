a83c33960271b1d164cbc3f3fd4cd32b6a783d98
Thomas Erdmenger <thomas.erdmenger@icloud.com>
2023-10-30 13:38:42 +0100

    Add step 2 in multistep form

--- src/components/MultistepForm.jsx
+++ src/components/MultistepForm.jsx
@@ -23,38 +23,96 @@ export default function MultistepForm() {
   const handlePreviousStep = () =>
     setCurrentStep((previousStep) => previousStep - 1);
 
-  return (
-    <form>
-      <h2>Personal Information</h2>
-      <div>
-        <label>Step {currentStep} of 3</label>
-        <progress value={currentStep} max={3}></progress>
-      </div>
-      <div>
-        <label htmlFor="name">Name</label>
-        <input
-          required
-          name="name"
-          id="name"
-          placeholder="Enter your name"
-          value={formData.name}
-          onChange={handleChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input
-          required
-          id="email"
-          name="email"
-          value={formData.email}
-          placeholder="Enter your email"
-          onChange={handleChange}
-        />
-      </div>
-      <button type="button" onClick={handleNextStep}>
-        Next
-      </button>
-    </form>
-  );
+  if (currentStep === 1) {
+    return (
+      <form>
+        <h2>Personal Information</h2>
+        <div>
+          <label>Step {currentStep} of 3</label>
+          <progress value={currentStep} max={3}></progress>
+        </div>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            name="name"
+            onChange={handleChange}
+            placeholder="Enter your name"
+            required
+            type="text"
+            value={formData.name}
+          />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            id="email"
+            name="email"
+            onChange={handleChange}
+            placeholder="Enter your email"
+            required
+            type="text"
+            value={formData.email}
+          />
+        </div>
+        <button type="button" onClick={handleNextStep}>
+          Next
+        </button>
+      </form>
+    );
+  } else if (currentStep === 2) {
+    return (
+      <form>
+        <h2>Address</h2>
+        <div>
+          <label>Step {currentStep} of 3</label>
+          <progress value={currentStep} max={3}></progress>
+        </div>
+        <div>
+          <label htmlFor="address">Address</label>
+          <input
+            id="address"
+            name="address"
+            onChange={handleChange}
+            placeholder="Enter your address"
+            required
+            type="text"
+            value={formData.address}
+          />
+        </div>
+        <div>
+          <label htmlFor="ciy">City</label>
+          <input
+            id="city"
+            name="city"
+            onChange={handleChange}
+            placeholder="Enter your city"
+            required
+            type="text"
+            value={formData.city}
+          />
+        </div>
+        <div>
+          <label htmlFor="zipcode">Zipcode</label>
+          <input
+            id="zipcode"
+            name="zipcode"
+            onChange={handleChange}
+            placeholder="Enter your Zipcode"
+            required
+            type="text"
+            value={formData.zipcode}
+          />
+        </div>
+        <div>
+          <button type="button" onClick={handlePreviousStep}>
+            Previous
+          </button>
+          <button type="button" onClick={handleNextStep}>
+            Next
+          </button>
+        </div>
+      </form>
+    );
+  }
 }
